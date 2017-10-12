740dec1aa899d3bd229759812dad22e0fd49312f
Ernesto <Zekyu@users.noreply.github.com>
2017-10-11 21:33:19 -0500

    Added TaxiDriver to Pojos

---
+++ app/src/main/java/edelafa/transportec/Transportec/back/Pojos/TaxiDriver.java
@@ -0,0 +1,21 @@
+package edelafa.transportec.Transportec.back.Pojos;
+
+/**
+ * Created by deadp on 11/10/2017.
+ */
+
+public class TaxiDriver extends BasePojo{
+
+    public String name;
+
+    public String lastName;
+
+    public String age;
+
+    public String address;
+
+    public String folioNumber;
+
+    public String phoneNumber;
+
+}
