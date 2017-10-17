5313217650e303281b905dc3a5bd2e0181e02d61
Ernesto <Zekyu@users.noreply.github.com>
2017-10-17 11:14:48 -0500

    Added Taxista and Tarifa layout

--- app/src/main/java/edelafa/transportec/Transportec/Activities/InfoTarifaActivity.java
+++ app/src/main/java/edelafa/transportec/Transportec/Activities/InfoTarifaActivity.java
@@ -1,8 +1,26 @@
 package edelafa.transportec.Transportec.Activities;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import edelafa.transportec.R;
+
 /**
  * Created by deadp on 15/09/2017.
  */
 
-public class InfoTarifaActivity {
+public class InfoTarifaActivity extends Activity {
+    @BindView(R.id.lvTarifa)
+    private ListView listViewTarifa;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_tarifa);
+        ButterKnife.bind(this);
+
+    }
 }
--- app/src/main/java/edelafa/transportec/Transportec/Activities/InfoTaxistaActivity.java
+++ app/src/main/java/edelafa/transportec/Transportec/Activities/InfoTaxistaActivity.java
@@ -1,6 +1,23 @@
 package edelafa.transportec.Transportec.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
 
-public class InfoTaxistaActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import edelafa.transportec.R;
+
+public class InfoTaxistaActivity extends Activity {
+
+    @BindView(R.id.lvTaxistas)
+    private ListView listViewTaxistas;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_taxista);
+        ButterKnife.bind(this);
+
+    }
 }
