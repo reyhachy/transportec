bf9ecb86c5a5b9e9f5f9ead25563e7b86398d28f
Juan Reynaldo Escobar Miron <jrescobarmiron@gmail.com>
2017-12-21 12:46:49 -0600

    modify resources

--- app/src/main/java/edelafa/transportec/Transportec/Activities/InfoTarifaActivity.java
+++ app/src/main/java/edelafa/transportec/Transportec/Activities/InfoTarifaActivity.java
@@ -1,8 +1,25 @@
 package edelafa.transportec.Transportec.Activities;
 
+import android.os.Bundle;
+
+import com.totalplay.mvp.BaseActivity;
+import com.totalplay.mvp.BasePresenter;
+
+import butterknife.ButterKnife;
+import edelafa.transportec.R;
+
 /**
  * Created by deadp on 01/11/2017.
  */
 
-public class InfoTarifaActivity {
+public class InfoTarifaActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_tarifa);
+        ButterKnife.bind(this);
+
+
+    }
 }
